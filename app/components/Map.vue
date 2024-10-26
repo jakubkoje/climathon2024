b9118070ffeae9b3d584776c4d4f52568f28fbcc
Jakub Jelínek <jakubkoje@gmail.com>
2024-10-26 21:58:05 +0200

    added map with dynamic data

---
+++ app/components/Map.vue
@@ -0,0 +1,211 @@
+<template>
+  <MapboxMap
+    style="position: relative; width: 100%; aspect-ratio: 3/2; border: 1px solid #ccc;"
+    map-id="map"
+    @click="onMapClick"
+    :options="{
+        style: 'mapbox://styles/jakubkoje/cm2pdg972007d01qwciof9hbn',
+        center: [17.150450, 48.157436],
+        zoom: 20
+      }"
+  />
+
+  <USlider :min="0" :max="359" :default-value="50" v-model="rotation" class="mt-4" />
+</template>
+
+<script setup lang="ts">
+const rotation = ref(0);
+
+let center = [17.150450, 48.157436];
+const latitudeOffset = ref(0.00002703);
+const longitudeOffset = ref(0.00007243);
+
+const boxCoordinates = computed(() => [
+  [center[0] - longitudeOffset.value / 2, center[1] - latitudeOffset.value / 2], // Bottom-left
+  [center[0] - longitudeOffset.value / 2, center[1] + latitudeOffset.value / 2], // Top-left
+  [center[0] + longitudeOffset.value / 2, center[1] + latitudeOffset.value / 2], // Top-right
+  [center[0] + longitudeOffset.value / 2, center[1] - latitudeOffset.value / 2], // Bottom-right
+  [center[0] - longitudeOffset.value / 2, center[1] - latitudeOffset.value / 2]  // Closing the loop
+]);
+
+const props = defineProps<{ container: number }>();
+
+const containers = {
+  1: {
+    image: 'sm.png',
+    latitudeOffset: 0.00002703,
+    longitudeOffset: 0.00007243
+  },
+  2: {
+    image: 'md.png',
+    latitudeOffset: 0.00003153,
+    longitudeOffset: 0.00007967
+  },
+  3: {
+    image: 'lg.png',
+    latitudeOffset: 0.00003604,
+    longitudeOffset: 0.00008691
+  },
+  4: {
+    image: 'xl.png',
+    latitudeOffset: 0.00004054,
+    longitudeOffset: 0.00009416
+  },
+};
+
+let originalCoords = [...boxCoordinates.value];
+
+useMapbox('map', (map) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = 200;
+  canvas.height = 200;
+  canvas.style.pointerEvents = 'none';
+  const context = canvas.getContext('2d');
+
+  const img = new Image();
+  img.onload = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.save();
+    context.translate(canvas.width / 2, canvas.height / 2);
+    context.rotate(rotation.value * (Math.PI / 180)); // Apply current rotation angle
+    context.translate(-canvas.width / 2, -canvas.height / 2);
+    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    context.restore();
+  };
+
+  img.src = containers[props.container].image;
+
+  map.addSource('canvas-source', {
+    type: 'canvas',
+    canvas: canvas,
+    coordinates: boxCoordinates.value.slice(0, 4)
+  });
+
+  map.addLayer({
+    id: 'canvas-layer',
+    type: 'raster',
+    source: 'canvas-source'
+  });
+
+  map.addSource('box', {
+    type: 'geojson',
+    data: {
+      type: 'Feature',
+      geometry: {
+        type: 'Polygon',
+        coordinates: [boxCoordinates.value]
+      }
+    }
+  });
+
+  map.addLayer({
+    id: 'box-layer',
+    type: 'fill',
+    source: 'box',
+    paint: {
+      'fill-color': '#0000FF',
+      'fill-opacity': 0
+    }
+  });
+
+  let isDragging = false;
+  let startCoords = null;
+
+  map.on('mousedown', 'box-layer', (e) => {
+    e.preventDefault();
+    isDragging = true;
+    startCoords = e.lngLat;
+  });
+
+  map.on('mousemove', (e) => {
+    if (!isDragging) return;
+
+    const dx = e.lngLat.lng - startCoords.lng;
+    const dy = e.lngLat.lat - startCoords.lat;
+
+    const translatedCoordinates = originalCoords.map(coord => [
+      coord[0] + dx,
+      coord[1] + dy
+    ]);
+
+    updateBoxAndCanvas(translatedCoordinates);
+    center = [e.lngLat.lng, e.lngLat.lat];
+  });
+
+  map.on('mouseup', () => {
+    isDragging = false;
+    originalCoords = map.getSource('box')._data.geometry.coordinates[0];
+  });
+
+  const getRotatedCoordinates = (center, angle) => {
+    const cos = Math.cos(toRadians(angle));
+    const sin = Math.sin(toRadians(angle));
+
+    return originalCoords.map(([lng, lat]) => {
+      const [x, y] = toLocalTangentPlane(lng, lat, center);
+      const xNew = x * cos - y * sin;
+      const yNew = x * sin + y * cos;
+
+      return fromLocalTangentPlane(xNew, yNew, center);
+    });
+  };
+
+  const rotateBox = (newAngle) => {
+    const newCoordinates = getRotatedCoordinates(center, newAngle);
+    updateBoxAndCanvas(newCoordinates);
+    originalCoords = newCoordinates;
+
+    // Redraw image with new rotation angle
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.save();
+    context.translate(canvas.width / 2, canvas.height / 2);
+    context.rotate(newAngle * (Math.PI / 180));
+    context.translate(-canvas.width / 2, -canvas.height / 2);
+    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    context.restore();
+  };
+
+  const updateBoxAndCanvas = (coordinates) => {
+    map.getSource('box').setData({
+      type: 'Feature',
+      geometry: {
+        type: 'Polygon',
+        coordinates: [coordinates]
+      }
+    });
+    map.getSource('canvas-source').setCoordinates(coordinates.slice(0, 4));
+  };
+
+  watch(rotation, (newValue, oldValue) => {
+    const angleDiff = newValue - oldValue;
+    rotateBox(angleDiff);
+  });
+
+  watch(
+    () => props.container,
+    (newContainer) => {
+      img.src = containers[newContainer].image;
+      latitudeOffset.value = containers[newContainer].latitudeOffset;
+      longitudeOffset.value = containers[newContainer].longitudeOffset;
+
+      const newCoordinates = [
+        [center[0] - longitudeOffset.value / 2, center[1] - latitudeOffset.value / 2],
+        [center[0] - longitudeOffset.value / 2, center[1] + latitudeOffset.value / 2],
+        [center[0] + longitudeOffset.value / 2, center[1] + latitudeOffset.value / 2],
+        [center[0] + longitudeOffset.value / 2, center[1] - latitudeOffset.value / 2],
+        [center[0] - longitudeOffset.value / 2, center[1] - latitudeOffset.value / 2]
+      ];
+
+      updateBoxAndCanvas(newCoordinates);
+      originalCoords = newCoordinates;
+
+      // Ensure new image is drawn with rotation
+      img.onload = () => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.save();
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      };
+    }
+  );
+});
+</script>
--- app/components/Step3.vue
+++ app/components/Step3.vue
@@ -1,81 +1,29 @@
 <!-- components/Step3.vue -->
 <template>
   <div>
-    <h2 class="text-2xl font-bold mb-2">Vyberte si typ kontajnerového stojiska</h2>
+    <h2 class="text-2xl font-bold mb-2">Vyberte si typ a umiestnenie kontajnerového stojiska</h2>
     <p class="mb-6">Veľkosť kontajnerového stojiska sa vyberá podľa počtu obyvateľov v bytovej jednotke. Na základe zadaného počtu obyvateľov sme pre vás vybrali odporúčané kontajnerové stojisko.</p>
-    <UForm :schema="v.safeParser(step3Schema)" :state="buildingState" @submit="submitStep" class="space-y-4">
+    <UForm :schema="v.safeParser(step3Schema)" :state="buildingState" @submit="submitStep" class="space-y-4 flex flex-col">
 
-      <UCard class="mb-4">
-        <div class="flex gap-8">
+      <UFormField label="Typ kontajnerového stojiska" name="container">
+      <UCard v-for="container in containerOptions" :key="container.id" @click="selectedContainer = container" class="mb-4 hover:bg-gray-50 cursor-pointer" :class="{'bg-green-100 hover:bg-green-100': selectedContainer.id === container.id}">
+        <div class="flex gap-8 items-center">
           <UAvatar src="https://placehold.co/400x400" class="rounded-md size-32" />
           <div>
             <h3 class="text-xl font-bold">
-              Kapacita CA 150 Obyvateľov
+              {{container.title}}
             </h3>
-            <ul class="list-disc list-inside">
-              <li>2x 1100L komunálny odpad</li>
-              <li>1x 1100L papier</li>
-              <li>1x 1100L plast</li>
-              <li>1x sklo</li>
-            </ul>
+            <div v-html="container.description" />
+          </div>
+          <div class="ml-auto" v-if="selectedContainer.id === container.id">
+            <UIcon name="i-heroicons-check-circle" class="text-green-500 size-12" />
           </div>
         </div>
       </UCard>
-      <UCard class="mb-4">
-        <div class="flex gap-8">
-          <UAvatar src="https://placehold.co/400x400" class="rounded-md size-32" />
-          <div>
-            <h3 class="text-xl font-bold">
-              Kapacita CA 150 Obyvateľov
-            </h3>
-            <ul class="list-disc list-inside">
-              <li>2x 1100L komunálny odpad</li>
-              <li>1x 1100L papier</li>
-              <li>1x 1100L plast</li>
-              <li>1x sklo</li>
-            </ul>
-          </div>
-        </div>
-      </UCard>
-      <UCard class="mb-4">
-        <div class="flex gap-8">
-          <UAvatar src="https://placehold.co/400x400" class="rounded-md size-32" />
-          <div>
-            <h3 class="text-xl font-bold">
-              Kapacita CA 150 Obyvateľov
-            </h3>
-            <ul class="list-disc list-inside">
-              <li>2x 1100L komunálny odpad</li>
-              <li>1x 1100L papier</li>
-              <li>1x 1100L plast</li>
-              <li>1x sklo</li>
-            </ul>
-          </div>
-        </div>
-      </UCard>
-      <UCard class="mb-4">
-        <div class="flex gap-8">
-          <UAvatar src="https://placehold.co/400x400" class="rounded-md size-32" />
-          <div>
-            <h3 class="text-xl font-bold">
-              Kapacita CA 150 Obyvateľov
-            </h3>
-            <ul class="list-disc list-inside">
-              <li>2x 1100L komunálny odpad</li>
-              <li>1x 1100L papier</li>
-              <li>1x 1100L plast</li>
-              <li>1x sklo</li>
-            </ul>
-          </div>
-        </div>
-      </UCard>
-
-      <UFormField label="Address" name="address">
-        <UInput v-model="buildingState.address" placeholder="Address" />
       </UFormField>
 
-      <UFormField label="Postal Code" name="postalCode">
-        <UInput v-model="buildingState.postalCode" placeholder="Postal Code" />
+      <UFormField label="Umiestnenie kontajnerového stojiska" name="map">
+        <Map :container="selectedContainer.id" />
       </UFormField>
       <UButton type="submit">Next Step</UButton>
     </UForm>
@@ -95,6 +43,36 @@ const buildingState = reactive({
   address: building.address,
   postalCode: building.postalCode
 });
+
+const containerOptions = ref([
+  { id: 1, title: 'Kapacita CA 60 Obyvateľov (5m x 3m)', description: `<ul class="list-disc list-inside">
+              <li>1x 1100L komunálny odpad</li>
+              <li>1x 1100L papier</li>
+              <li>1x 1100L plast</li>
+              <li>1x sklo</li>
+            </ul>` },
+  { id: 2, title: 'Kapacita CA 150 Obyvateľov (5.5m x 3.5m)', description: `<ul class="list-disc list-inside">
+              <li>2x 1100L komunálny odpad</li>
+              <li>1x 1100L papier</li>
+              <li>1x 1100L plast</li>
+              <li>1x sklo</li>
+            </ul>` },
+  { id: 3, title: 'Kapacita CA 200 Obyvateľov (6m x 4m)', description: `<ul class="list-disc list-inside">
+              <li>2x 1100L komunálny odpad</li>
+              <li>2x 1100L papier</li>
+              <li>2x 1100L plast</li>
+              <li>1x sklo</li>
+            </ul>` },
+  { id: 4, title: 'Kapacita CA 200 Obyvateľov (6.5m x 4.5m)', description: `<ul class="list-disc list-inside">
+              <li>2x 1100L komunálny odpad</li>
+              <li>2x 1100L papier</li>
+              <li>2x 1100L plast</li>
+              <li>1x sklo</li>
+              <li>1x textil</li>
+            </ul>` },
+]);
+
+const selectedContainer = ref(containerOptions.value[0]);
 
 const increment = (key) => {
   layout[key]++;
