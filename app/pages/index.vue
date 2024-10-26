72a86773e26f24c164854ce891d372fbd576bc8d
Jakub Jelínek <jakubkoje@gmail.com>
2024-10-26 14:44:01 +0200

    map

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,9 @@
   <h1>Hello world!</h1>
   <UButton>Click me</UButton>
   <MapboxMap
-    style="position: relative; width: 100%; height: 400px; border: 1px solid #ccc;"
+    style="position: relative; width: 100%; height: 900px; border: 1px solid #ccc;"
     map-id="map"
+    @click="onMapClick"
     :options="{
         style: 'mapbox://styles/jakubkoje/cm2pdg972007d01qwciof9hbn', // style URL
         center: [17.107748, 48.148598], // starting position
@@ -18,4 +19,8 @@ const {data} = await useFetch('/api/waters', {server: false})
 watch(data, () => {
   console.log(data.value[0].geojson)
 })
+
+const onMapClick = (e: any) => {
+  console.log(e.lngLat)
+}
 </script>
