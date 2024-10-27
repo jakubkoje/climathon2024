33dc4966b87da526fe7a9a33ce2ff4d3b19ee302
RikoAppDev <riko.appdev@gmail.com>
2024-10-27 10:32:44 +0100

    code refactoring

--- server/api/storage/index.ts
+++ server/api/storage/index.ts
@@ -4,75 +4,75 @@ import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { collection, addDoc } from 'firebase/firestore';
 
 export default defineEventHandler(async (event) => {
-  const requestData = await readBody(event);
-  console.log(requestData)
+    const requestData = await readBody(event);
+    console.log(requestData)
 
-  if (requestData?.formData?.building?.map && requestData.formData.building.map.length ) {
-    const data = requestData.formData.building.map
+    if (requestData?.formData?.step3?.map && requestData.formData.step3.map.length) {
+        const data = requestData.formData.step3.map
 
-    let helper = { ...data }      ;
+        let helper = { ...data };
 
-    console.log(helper)
-
-
-    requestData.formData.building.map = helper
-  }
+        console.log(helper)
 
 
-  // const body = await readMultipartFormData(event);
-
-  // Assume the image file is the first item in form data
-  // const file = body?.[0];
-  // if (!file) {
-  //   throw createError({ statusCode: 400, message: 'No file uploaded' });
-  // }
-
-  // const fileBuffer = file.data;
-  // const fileName = `${crypto.randomUUID()}-${file.filename}`;
+        requestData.formData.step3.map = helper
+    }
 
 
-  // Create a storage reference
-  // const storageRef = ref(storage, `images/${fileName}`);
+    // const body = await readMultipartFormData(event);
 
-  // const requestData = {
-  //   image: {
-  //     imageFileName: fileName,
-  //     path: ''
-  //   },
-  //   administrator: {
-  //     name: 'Lukas Nemec',
-  //     email: 'nemec@pucwoll.com',
-  //     phoneNumber: '+321321321312'
-  //   },
-  //   flat: {
-  //     address: 'Prievozska 11A, 022 32 Bratislava',
-  //     noRezidents: 56
-  //   },
-  //   container: {
-  //     type: 'XL',
-  //     cords: {
-  //       lan: 47.543,
-  //       lat: 45.432
-  //     }
-  //   }
-  // }
+    // Assume the image file is the first item in form data
+    // const file = body?.[0];
+    // if (!file) {
+    //   throw createError({ statusCode: 400, message: 'No file uploaded' });
+    // }
+
+    // const fileBuffer = file.data;
+    // const fileName = `${crypto.randomUUID()}-${file.filename}`;
 
 
-  try {
-    // Upload file to Firebase Storage
-    // const snapshot = await uploadBytes(storageRef, fileBuffer);
-    // const downloadURL = await getDownloadURL(snapshot.ref);
+    // Create a storage reference
+    // const storageRef = ref(storage, `images/${fileName}`);
 
-    // add download path to db item data 
-    // requestData.image.path = downloadURL
+    // const requestData = {
+    //   image: {
+    //     imageFileName: fileName,
+    //     path: ''
+    //   },
+    //   administrator: {
+    //     name: 'Lukas Nemec',
+    //     email: 'nemec@pucwoll.com',
+    //     phoneNumber: '+321321321312'
+    //   },
+    //   flat: {
+    //     address: 'Prievozska 11A, 022 32 Bratislava',
+    //     noRezidents: 56
+    //   },
+    //   container: {
+    //     type: 'XL',
+    //     cords: {
+    //       lan: 47.543,
+    //       lat: 45.432
+    //     }
+    //   }
+    // }
 
-    // Add new document to the 'requests' collection
-    const docRef = await addDoc(collection(db, 'requests'), requestData);
 
-    return { success: true, entryId: docRef.id };
-    // return { success: true, downloadURL, entryId: docRef.id };
-  } catch (error) {
-    console.error('Error uploading image:', error);
-    throw createError({ statusCode: 500, message: 'Image upload failed or failed to save request' });
-  }
+    try {
+        // Upload file to Firebase Storage
+        // const snapshot = await uploadBytes(storageRef, fileBuffer);
+        // const downloadURL = await getDownloadURL(snapshot.ref);
+
+        // add download path to db item data
+        // requestData.image.path = downloadURL
+
+        // Add new document to the 'requests' collection
+        const docRef = await addDoc(collection(db, 'requests'), requestData);
+
+        return { success: true, entryId: docRef.id };
+        // return { success: true, downloadURL, entryId: docRef.id };
+    } catch (error) {
+        console.error('Error uploading image:', error);
+        throw createError({ statusCode: 500, message: 'Image upload failed or failed to save request' });
+    }
 });
